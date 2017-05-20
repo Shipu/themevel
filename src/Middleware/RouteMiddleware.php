e9349d4be3e0573975dc75c220afbf8e95b697c1
Shipu Ahamed <shipuahamed01@gmail.com>
2017-05-20 12:23:57 +0000

    Apply fixes from StyleCI

--- src/Middleware/RouteMiddleware.php
+++ src/Middleware/RouteMiddleware.php
@@ -9,16 +9,16 @@ class RouteMiddleware
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request $request
-     * @param  \Closure $next
-     * @param  string $themeName
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     * @param string                   $themeName
      *
      * @return mixed
      */
-    public function handle( $request, Closure $next, $themeName )
+    public function handle($request, Closure $next, $themeName)
     {
         \Theme::set($themeName);
-        
+
         return $next($request);
     }
 }
