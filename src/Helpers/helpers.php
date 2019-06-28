431b844e2c2751d761b9cce5bea26bf6f2f7f6d8
Shipu Ahamed <shipuahamed01@gmail.com>
2019-06-28 17:40:01 +0600

    add new helper and laravel mix support
    add new helper and laravel mix support

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -15,6 +15,35 @@ if (!function_exists('themes')) {
     }
 }
 
+if (!function_exists('theme_mix')) {
+    /**
+     * Get the current theme path to a versioned Mix file.
+     *
+     * @param string $path
+     * @param  string  $manifestDirectory
+     *
+     * @return \Illuminate\Support\HtmlString|string
+     */
+    function theme_mix($path, $manifestDirectory = '')
+    {
+        return Theme::themeMix($path, $manifestDirectory);
+    }
+}
+
+if (!function_exists('theme_path')) {
+    /**
+     * Get the current theme path to a versioned Mix file.
+     *
+     * @param string $path
+     *
+     * @return string
+     */
+    function theme_path($path)
+    {
+        return Theme::getFullPath($path);
+    }
+}
+
 if (!function_exists('lang')) {
     /**
      * Get lang content from current theme.
@@ -26,5 +55,33 @@ if (!function_exists('lang')) {
     function lang($fallback)
     {
         return Theme::lang($fallback);
+    }
+}
+
+if (!function_exists('current_theme_name')) {
+    /**
+     * Get current active theme name only or themeinfo collection.
+     *
+     * @param bool $collection
+     *
+     * @return null|ThemeInfo
+     */
+    function current_theme_name()
+    {
+        return Theme::current();
+    }
+}
+
+if (!function_exists('current_theme')) {
+    /**
+     * Get current active theme name only or themeinfo collection.
+     *
+     * @param bool $collection
+     *
+     * @return null|ThemeInfo
+     */
+    function current_theme($collection = false)
+    {
+        return Theme::current($collection);
     }
 }
